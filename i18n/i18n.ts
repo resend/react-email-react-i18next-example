4f96770d84d414e5a9f30b115af651bfc93b3aae
gabriel miranda <gabrielmfern@outlook.com>
2026-01-12 15:06:45 -0300

    lint, add spanish to valid locales

--- i18n/i18n.ts
+++ i18n/i18n.ts
@@ -1,13 +1,14 @@
-import i18next from 'i18next';
-import resourcesToBackend from 'i18next-resources-to-backend';
-import { initReactI18next } from 'react-i18next';
+import i18next from 'i18next'
+import resourcesToBackend from 'i18next-resources-to-backend'
+import { initReactI18next } from 'react-i18next/initReactI18next'
 
 export const validLocales = ['en', 'es', 'pt'] as const;
 export type Locale = (typeof validLocales)[number];
 
 i18next
   .use(initReactI18next)
-  .use(resourcesToBackend((language: Locale, namespace: string) => import(`./messages/${language}/${namespace}.json`))).init({
+  .use(resourcesToBackend((language: string, namespace: string) => import(`./messages/${language}/${namespace}.json`)))
+  .init({
     supportedLngs: validLocales,
     fallbackLng: 'en',
     lng: undefined,
